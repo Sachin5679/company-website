6170cb1ecbfde9b41fe44ba4a6a6c65d9d6f2090
Sachin Jayadev <sachinjayadev18@gmail.com>
2024-03-19 12:05:07 +0530

    responsiveness

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -12,7 +12,7 @@ const ServiceCard = ({ title, icon: IconComponent, text }: Service) => {
   return (
     <div className="flex flex-col items-center p-8 m-2 border rounded-xl shadow-xl bg-white hover:bg-green-100 transition-all">
       <IconComponent className="text-7xl mb-2 text-red-500" />
-      <div className="text-2xl font-bold mb-2">{title}</div>
+      <div className="md:text-2xl text-xl font-bold mb-2">{title}</div>
       <p className="text-sm opacity-50">{text}</p>
     </div>
   );
