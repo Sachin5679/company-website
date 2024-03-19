c229d1a81488fdbd0758c85de80498fae460f6f5
Sachin Jayadev <sachinjayadev18@gmail.com>
2024-03-19 10:58:59 +0530

    removed team

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from '@/components/Hero'
 import About from '@/components/About';
 import Services from '@/components/Services';
 import Profile from '@/components/Profile';
-import Team from '@/components/Team';
+// import Team from '@/components/Team';
 import ContactUs from '@/components/ContactUs';
 import '@fontsource/jost';
 
