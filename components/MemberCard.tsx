460bd4880fce01bb754e8ed5745a69978c9e06ae
Sachin Jayadev <sachinjayadev18@gmail.com>
2024-03-19 10:56:59 +0530

    temporarily removed team page

--- components/MemberCard.tsx
+++ components/MemberCard.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons';
+// import React from 'react'
+// import { SocialIcon } from 'react-social-icons';
 
-interface Member {
-  name: string;
-  image: string;
-  post: string;
-  linkedin: string;
-}
+// interface Member {
+//   name: string;
+//   image: string;
+//   post: string;
+//   linkedin: string;
+// }
 
-const MemberCard = ({name, image, post, linkedin}: Member) => {
-  return (
-    <div className="flex flex-col items-center p-8 m-2 border rounded-xl shadow-xl bg-white hover:bg-green-100 transition-all w-1/3">
-      {/* <FontAwesomeIcon icon={faLeaf} className="text-green-500 text-3xl mb-4" /> */}
-      <img className='rounded-full' src="p4.jpeg" alt="" />
-      <div className="text-4xl font-extrabold mb-2">Name</div>
-      <p className="text-2xl text-gray-600 opacity-60">Designation</p>
-      <SocialIcon className="hover:scale-125 transition-all" target="_blank" url="https://www.linkedin.com/in/sachin-jayadev-010492232/" fgColor="gray" title="LinkedIn" bgColor="transparent"/>
-    </div>
-  )
-}
+// const MemberCard = ({name, image, post, linkedin}: Member) => {
+//   return (
+//     <div className="flex flex-col items-center p-8 m-2 border rounded-xl shadow-xl bg-white hover:bg-green-100 transition-all w-1/3">
+//       {/* <FontAwesomeIcon icon={faLeaf} className="text-green-500 text-3xl mb-4" /> */}
+//       <img className='rounded-full' src="p4.jpeg" alt="" />
+//       <div className="text-4xl font-extrabold mb-2">Name</div>
+//       <p className="text-2xl text-gray-600 opacity-60">Designation</p>
+//       <SocialIcon className="hover:scale-125 transition-all" target="_blank" url="https://www.linkedin.com/in/sachin-jayadev-010492232/" fgColor="gray" title="LinkedIn" bgColor="transparent"/>
+//     </div>
+//   )
+// }
 
-export default MemberCard
+// export default MemberCard
--- components/Team.tsx
+++ components/Team.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import MemberCard from './MemberCard';
+// import React, { useState } from 'react';
+// import MemberCard from './MemberCard';
 
-const Team = () => {
+// const Team = () => {
 
-  return (
-    <section id='team' className="h-3/4 w-screen p-12">
-        <div className='text-center'>
-            <div className=''>
-                <h1 className='text-7xl font-black'>OUR TEAM</h1>
-                {/* <hr className="w-16 mx-auto border-t-2 border-gray-400 mb-4" /> */}
-            </div>
-            <div className='flex justify-center items-center'>
-                <MemberCard />
-                <MemberCard />
-                <MemberCard />
-            </div>
-        </div>
+//   return (
+//     <section id='team' className="h-3/4 w-screen p-12">
+//         <div className='text-center'>
+//             <div className=''>
+//                 <h1 className='text-7xl font-black'>OUR TEAM</h1>
+//                 {/* <hr className="w-16 mx-auto border-t-2 border-gray-400 mb-4" /> */}
+//             </div>
+//             <div className='flex justify-center items-center'>
+//                 <MemberCard />
+//                 <MemberCard />
+//                 <MemberCard />
+//             </div>
+//         </div>
 
-    </section>
-  );
-};
+//     </section>
+//   );
+// };
 
-export default Team;
+// export default Team;
